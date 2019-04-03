f809f7afd0cd5de0cb3a64031a1e24bd6272c4ec
mekatama <tamatte@hotmail.com>
2019-04-03 18:43:41 +0900

    rigidbodyで制御実験

---
+++ TrainController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainController : MonoBehaviour {
+	public float speed = 3f;	//
+	float moveZ = 0f;			//
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();		
+	}
+	
+	void Update () {
+		moveZ = Input.GetAxis("Horizontal") * speed;
+//		Vector3 direction = new Vector3(0,0,moveZ);
+	}
+
+	void FixedUpdate(){
+		rb.velocity = new Vector3(0,0,moveZ);
+	}
+
+}
