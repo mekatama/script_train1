5c92668cb324d676328527e9ead9d9b86a63a6a4
mekatama <tamatte@hotmail.com>
2019-04-08 18:47:06 +0900

    コメント追加

--- TrainController.cs
+++ TrainController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrainController : MonoBehaviour {
-	public float speed = 3f;	//
-	float moveZ = 0f;			//
-	Rigidbody rb;
+	public float speed = 3f;	//基本速度
+	float moveZ = 0f;			//実際の速度
+	Rigidbody rb;				//rigidbody格納用
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();		
@@ -13,11 +13,12 @@ public class TrainController : MonoBehaviour {
 	
 	void Update () {
 		moveZ = Input.GetAxis("Horizontal") * speed;
-//		Vector3 direction = new Vector3(0,0,moveZ);
 	}
 
 	void FixedUpdate(){
-		rb.velocity = new Vector3(0,0,moveZ);
+		//y方向にゼロを入れると落下速度がリセットされるので注意
+		rb.velocity = new Vector3(0,rb.velocity.y,moveZ);
+		rb.AddForce (Vector3.down * 50, ForceMode.Acceleration);
 	}
 
 }
