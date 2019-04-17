678bdb34df69e66d357e714723dba3a6dc8965f3
mekatama <tamatte@hotmail.com>
2019-04-17 19:13:02 +0900

    speed調整できるように

--- TrainController.cs
+++ TrainController.cs
@@ -6,13 +6,18 @@ public class TrainController : MonoBehaviour {
 	public float speed = 3f;	//基本速度
 	float moveZ = 0f;			//実際の速度
 	Rigidbody rb;				//rigidbody格納用
+	GameObject gameController;	//検索したオブジェクト入れる用
 
 	void Start () {
-		rb = GetComponent<Rigidbody>();		
+		rb = GetComponent<Rigidbody>();								//rigidbody格納
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
 	}
 	
 	void Update () {
-		moveZ = Input.GetAxis("Horizontal") * speed;
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//ボタン入力でspeed制御
+		moveZ = speed * gc.speedType;
 	}
 
 	void FixedUpdate(){
@@ -20,5 +25,4 @@ public class TrainController : MonoBehaviour {
 		rb.velocity = new Vector3(0,rb.velocity.y,moveZ);
 		rb.AddForce (Vector3.down * 50, ForceMode.Acceleration);
 	}
-
 }
